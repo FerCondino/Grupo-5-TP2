420e77a6c9f0063c2c39f308a837db50132732df
sarahalfonsin <sarahalfonsin@hotmail.com>
2022-11-24 19:30:18 -0300

    Subo lectura de audios automatizada con nombre ruta

--- main/tp2.py
+++ main/tp2.py
@@ -18,24 +18,25 @@ def lectura():
             row=row.split(',')
             datos.append({'id':id,'Timestamp':row[0],'Telefono_celular':row[1],'coord_latitud':row[2],'coord_longitud':row[3],'ruta_foto':row[4],'descripcion_texto':row[5],'ruta_Audio':row[6][:len(row[6])-1]})
     print(datos)
+    return datos
 
-def transcribir_audio():
+def transcribir_audio(datos):
     os.chdir("audios")
-    AUDIO = 'audioprueba1.wav'
+    for i in datos:
+        AUDIO:str= i['ruta_Audio']
+        recgnizer = sr.Recognizer()
+        with sr.AudioFile(AUDIO) as source:
+            audio = recgnizer.record(source)
 
-    recgnizer = sr.Recognizer()
-    with sr.AudioFile(AUDIO) as source:
-        audio = recgnizer.record(source)
-
-    try:
-        print("Google Speech Recognition thinks you said " + recgnizer.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        try:
+            print("Google Speech Recognition thinks you said " + recgnizer.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 def main():
-    lectura()
-    transcribir_audio()
+    datos=lectura()
+    transcribir_audio(datos)
 main()
