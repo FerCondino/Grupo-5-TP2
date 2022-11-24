6e164bb057489a10a5236aa93d858230090491c1
unknown <fcondino@fi.uba.ar>
2022-11-24 20:09:21 -0300

    agrego espacio entre TP2 y (2C2022)

--- main/tp2.py
+++ main/tp2.py
@@ -8,7 +8,7 @@ import os
 def lectura():
     datos : list[dict]=[]
     id:int=0
-    os.chdir("../TP2(2C2022)/main")
+    os.chdir("../TP2 (2C2022)/main")
     nombre_archivo ="reclamos.csv"
     with open( nombre_archivo,"r") as archivo:
         lector = csv.reader(archivo, delimiter=",")
