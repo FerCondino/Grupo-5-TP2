86f265dd660b7920f5d6e4e93adbdf5b2cb20bb9
MarcosCovino <covinomarcos2002@gmail.com>
2022-11-25 20:34:44 -0300

    Crea el nuevo csv

--- main/tp2.py
+++ main/tp2.py
@@ -1,14 +1,12 @@
-
-import speech_recognition as sr
+#import speech_recognition as sr
 from os import path
 import csv
 import os
 
 
-def lectura():
-    datos : list[dict]=[]
+def lectura(datos) -> None:
     id:int=0
-    os.chdir("..\Grupo-5-TP2/main")
+    
     nombre_archivo ="reclamos.csv"
     with open( nombre_archivo,"r") as archivo:
         lector = csv.reader(archivo, delimiter=",")
@@ -17,8 +15,7 @@ def lectura():
             id+=1
             row=row.split(',')
             datos.append({'id':id,'Timestamp':row[0],'Telefono_celular':row[1],'coord_latitud':row[2],'coord_longitud':row[3],'ruta_foto':row[4],'descripcion_texto':row[5],'ruta_Audio':row[6][:len(row[6])-1]})
-    print(datos)
-    return datos
+   
 
 def transcribir_audio(datos):
     os.chdir("audios")
@@ -36,7 +33,22 @@ def transcribir_audio(datos):
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
+def guardar_datos(datos:list) -> None:
+
+    archivo: str = 'BaseDenuncias.csv'
+    campos: tuple = ('Timestamp', 'Teléfono', 'Dirección_infracción', 'Localidad', 'Provincia', 'patente', 'descripción_texto', 'descripción_audio')
+
+    with open(archivo,"w",newline = '') as f:
+        csv_writer = csv.writer(f,delimiter = ",")
+        csv_writer.writerow(campos)
+        for denuncia in datos:
+            csv_writer.writerow((denuncia["id"], denuncia["Timestamp"], denuncia["Telefono_celular"], denuncia["descripcion_texto"]))
+
+
 def main():
-    datos=lectura()
-    transcribir_audio(datos)
+    datos : list[dict]=[]
+    lectura(datos)
+    guardar_datos(datos)
+    #transcribir_audio(datos)
+    
 main()
